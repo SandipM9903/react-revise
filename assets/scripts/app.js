0816ae501292ad662d78676c6ab30b30c8961b66
Sandip Mondal <72807288+SandipM9903@users.noreply.github.com>
2023-11-15 13:55:15 +0530

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,7 @@
+import { apikey } from "./util";
+import * as util from "./util";
+import name from "./util";
+
+console.log(apikey);
+console.log(name);
+console.log(util.abc);
---
+++ assets/scripts/util.js
@@ -0,0 +1,3 @@
+export let apikey = "abcdefgh";
+export default "sandip";
+export let abc = "ABC";
